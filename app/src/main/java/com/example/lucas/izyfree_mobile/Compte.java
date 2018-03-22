61fb3a5fab20a8c377c0d7ad2d7aaff98e7c3475
Sarah BERTIN <bertins@acajou15.iut-infobio.priv.univ-lille1.fr>
2018-03-22 10:33:07 +0100

    mon compte

---
+++ app/src/main/java/com/example/lucas/izyfree_mobile/Compte.java
@@ -0,0 +1,15 @@
+package com.example.lucas.izyfree_mobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Compte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compte);
+    }
+
+
+}
