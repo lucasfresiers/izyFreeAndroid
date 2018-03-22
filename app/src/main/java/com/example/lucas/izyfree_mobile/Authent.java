c16854845991739febaf83473bafea6dea50696c
Lucas Fresiers <LUCAS@MacBook-Pro-de-Lucas.local>
2018-03-22 11:24:46 +0100

    page authent ok

--- app/src/main/java/com/example/lucas/izyfree_mobile/Authent.java
+++ app/src/main/java/com/example/lucas/izyfree_mobile/Authent.java
@@ -1,5 +1,6 @@
 package com.example.lucas.izyfree_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class Authent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authent);
+        Intent intent = getIntent();
+        // On suppose que tu as mis un String dans l'Intent via le putExtra()
+        String value = intent.getStringExtra("chaine");
+
+
+
     }
 }
