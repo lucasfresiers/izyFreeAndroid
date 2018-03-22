901e91070aedbadfd1eab3c4fa30be222b6ad3b9
Lucas Fresiers <LUCAS@MacBook-Pro-de-Lucas.local>
2018-03-22 10:53:01 +0100

    new launch

--- app/src/main/java/com/example/lucas/izyfree_mobile/Welcome.java
+++ app/src/main/java/com/example/lucas/izyfree_mobile/Welcome.java
@@ -1,7 +1,9 @@
 package com.example.lucas.izyfree_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void onClickFreelance (View view) {
+        Intent intent = new Intent(this, Authent.class);
+        intent.putExtra("profil", "freelance");
+        startActivity(intent);
+    }
+    public void onClickEntreprise (View view) {
+        Intent intent = new Intent(this, Authent.class);
+        intent.putExtra("profil", "entreprise");
+        startActivity(intent);
     }
 }
